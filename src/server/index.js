4eacac4c34bdf6085b4ea45e6bb69524bc3c904f
unknown <ohndaniel@gmail.com>
2020-05-04 04:53:58 -0700

    Add Create, List, and Delete products.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,31 @@
 const stripe = require("stripe")(process.env.SK_TEST_KEY);
 
+// Create Customers
 stripe.customers
   .create({
     email: "customer@example.com",
   })
   .then((customer) => console.log('customer id: ' + customer.id))
   .catch((err) => console.error(err));
+
+
+// // Create
+// stripe.products.create(
+//   {
+//     name: 'shirt',
+//     type: 'good',
+//     description: 'comfy shirt',
+//     attributes: ['size', 'gender'],
+//   }).catch( err => console.log(err));
+
+
+
+// List
+stripe.products.list(
+).then( lst => console.log(lst));
+
+
+// // Delete
+// stripe.products.del(
+//   'prod_HDTkuwDpTVnaSf'
+// )
