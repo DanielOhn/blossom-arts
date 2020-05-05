077b0306c09faf21c758f6bdfc1060bb82083d0e
catsby <ohndaniel@gmail.com>
2020-05-04 23:39:10 -0700

    Add React Client. Add ExpressJS for server.

---
+++ src/client/src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "../styles/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+//TODO
+// List Products
+// Create Product
+// Edit Product
+// Delete Product
+// View Product
+
+// CRUD for Customers
--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,38 @@
+// // Create Customers
+// stripe.customers
+//   .create({
+//     email: "customer@example.com",
+//   })
+//   .then((customer) => console.log("customer id: " + customer.id))
+//   .catch((err) => console.error(err))
+
+// // // Create
+// // stripe.products.create(
+// //   {
+// //     name: 'shirt',
+// //     type: 'good',
+// //     description: 'comfy shirt',
+// //     attributes: ['size', 'gender'],
+// //   }).catch( err => console.log(err));
+
+// // // Delete
+// // stripe.products.del(
+// //   'prod_HDTkuwDpTVnaSf'
+// // )
+
+const express = require("express")
+const app = express()
+const port = 3001
+
 const stripe = require("stripe")(process.env.SK_TEST_KEY)
 
-// Create Customers
-stripe.customers
-  .create({
-    email: "customer@example.com",
-  })
-  .then((customer) => console.log("customer id: " + customer.id))
-  .catch((err) => console.error(err))
-
-// // Create
-// stripe.products.create(
-//   {
-//     name: 'shirt',
-//     type: 'good',
-//     description: 'comfy shirt',
-//     attributes: ['size', 'gender'],
-//   }).catch( err => console.log(err));
-
 // List
-stripe.products.list().then((lst) => console.log(lst))
+// const products = stripe.products.list().then((lst) => console.log(lst))
 
-// // Delete
-// stripe.products.del(
-//   'prod_HDTkuwDpTVnaSf'
-// )
+app.get("/", (req, res) => res.send("Hello World!"))
+
+app.get("/products", (req, res) => {
+  stripe.products.list().then((lst) => res.send(lst))
+})
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
