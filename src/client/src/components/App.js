d4471c180e95c3760f1203c131800fb07b0eca39
catsby <ohndaniel@gmail.com>
2020-05-05 01:17:35 -0700

    Add axios to pull data from server.  Iterate through products to display their info.

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "../styles/App.css"
 
+import axios from "axios"
+
 function App() {
+  const [products, setProducts] = useState()
+
+  useEffect(() => {
+    axios.get("/products").then((res) => {
+      setProducts(res.data.data)
+      console.log(res.data.data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-      </header>
+      <h1>Title of Page!</h1>
+      <p>Nav Bar Goes here</p>
+      <div>
+        <h3>Products</h3>
+        {products &&
+          Object.keys(products).map((i) => {
+           let product = products[i]
+
+           return <p key={product.id}>{product.name}</p>
+          })}
+      </div>
     </div>
   )
 }
