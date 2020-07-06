fb774e19ee8455ef0b3213cabc29c38a267f2c74
unknown <ohndaniel@gmail.com>
2020-07-06 04:03:02 -0700

    Update landing page to include all 3 sections.

--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -5,19 +5,60 @@ import axios from "axios"
 import Cart from "../icons/cart"
 import flower from "../images/flower-art.jpg"
 
+import Heart from "../icons/heart"
+import Puzzle from "../icons/puzzle"
+import Pencil from "../icons/pencil"
+
 function Home() {
+  const [answer, setAnswer] = useState(false)
+
+  function showAnswer() {
+    setAnswer(!answer)
+  }
+
   return (
     <div className="home">
       <div className="cover">
-        <div className="title">
+        <div className="image"></div>
+        <div className="title small">
           <h1>WELCOME</h1>
         </div>
-        <div className="text">
+        <div className="title large">
           <p>WHERE CREATIVITY COMES TO LIFE</p>
         </div>
       </div>
-      <div className="icons"></div>
-      <div className="faq"></div>
+      <div className="icons">
+        <div className="icon">
+          <Heart />
+          <h3>Lorem</h3>
+          <p>This is a full sentence that goes here.</p>
+        </div>
+        <div className="icon">
+          <Puzzle />
+          <h3>Lorem</h3>
+          <p>This is a full sentence that goes here.</p>
+        </div>
+        <div className="icon">
+          <Pencil />
+          <h3>Lorem</h3>
+          <p>This is a full sentence that goes here.</p>
+        </div>
+      </div>
+      <div className="faq">
+        <h1>FAQ</h1>
+
+        <div className="faq-section">
+          <div className="question">
+            <p>Yes, no maybe? I don't know. Can you repeat the question?</p>
+            <button onClick={showAnswer}>></button>
+          </div>
+          {answer && (
+            <div className="answer">
+              <p>You're not the boss of me now, and you're not so big!</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
